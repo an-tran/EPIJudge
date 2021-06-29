5745a83cbb103322d20b78cf69ffd428b1db5395
an.tran <an.tran@big-labs.com>
2021-06-29 22:54:16 +0700

    5.1 Parity of a word

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -5,7 +5,15 @@ public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
     // TODO - you fill in here.
-    return 0;
+    short count = 0;
+    while (x != 0) {
+      long y = x & ~(x - 1);
+      if (y > 0) {
+        count++;
+        x = x/(y * 2);
+      }
+    }
+    return (short) (count % 2);
   }
 
   public static void main(String[] args) {
