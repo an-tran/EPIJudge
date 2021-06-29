7c5b463892885ea2be7618b45f882d3ad5b9c3cc
an.tran <an.tran@big-labs.com>
2021-06-29 22:59:48 +0700

    5.1 Parity of a word (take 2: shorter version)

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -6,14 +6,19 @@ public class Parity {
   public static short parity(long x) {
     // TODO - you fill in here.
     short count = 0;
+//    while (x != 0) {
+//      long y = x & ~(x - 1);
+//      if (y > 0) {
+//        count++;
+//        x = x/(y * 2);
+//      }
+//    }
+    short result = 0;
     while (x != 0) {
-      long y = x & ~(x - 1);
-      if (y > 0) {
-        count++;
-        x = x/(y * 2);
-      }
+      result ^= 1;
+      x &= (x - 1);
     }
-    return (short) (count % 2);
+    return result;
   }
 
   public static void main(String[] args) {
