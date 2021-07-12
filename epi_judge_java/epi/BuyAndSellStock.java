7ea0fdced8ac16ca41538ac2eada537e1d25d3cd
an-tran <an.tran@big-labs.com>
2021-07-12 17:22:26 +0700

    6.6 Buy/Sell Stock: case analysis

--- epi_judge_java/epi/BuyAndSellStock.java
+++ epi_judge_java/epi/BuyAndSellStock.java
@@ -6,8 +6,26 @@ import java.util.List;
 public class BuyAndSellStock {
   @EpiTest(testDataFile = "buy_and_sell_stock.tsv")
   public static double computeMaxProfit(List<Double> prices) {
-    // TODO - you fill in here.
-    return 0.0;
+    // Invariants
+    // low: current lowest price
+    // i: current index
+    // profitMax: current max profit at [i-1]
+    // prices[i] < low => update new low
+    // prices[i] - low >  profitMax => update profitMax
+    double low = prices.get(0);
+    double high = Double.NEGATIVE_INFINITY;
+    double profitMax = 0d;
+    for (int i = 1; i < prices.size(); i++) {
+      if (Double.compare(prices.get(i),low) < 0)
+        low = prices.get(i);
+      else {
+        if (profitMax < (prices.get(i) - low)) {
+          profitMax = prices.get(i) - low;
+        }
+      }
+    }
+
+    return profitMax;
   }
 
   public static void main(String[] args) {
