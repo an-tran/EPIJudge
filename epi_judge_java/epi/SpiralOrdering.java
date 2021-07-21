5457bef1d68cd1fe629fc45dff9a2194325e2496
an.tran <an.tran@big-labs.com>
2021-07-21 23:48:51 +0700

    6.17 naive implementation

--- epi_judge_java/epi/SpiralOrdering.java
+++ epi_judge_java/epi/SpiralOrdering.java
@@ -2,6 +2,7 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.List;
 public class SpiralOrdering {
   @EpiTest(testDataFile = "spiral_ordering.tsv")
@@ -9,7 +10,32 @@ public class SpiralOrdering {
   public static List<Integer>
   matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
     // TODO - you fill in here.
-    return null;
+    int nr = squareMatrix.size();
+    List<Integer> ret = new ArrayList<>();
+    int i = 0, j = 0;
+    while (nr > 0 ) {
+      int n = 0;
+      while (n++ < nr) {
+        ret.add(squareMatrix.get(i).get(j++));
+      }
+      j--;
+      n = nr - 1;
+      while (n-- > 0) {
+        ret.add(squareMatrix.get(++i).get(j));
+      }
+
+      n = nr - 1;
+      while (n-- > 0) {
+        ret.add(squareMatrix.get(i).get(--j));
+      }
+      n = nr - 2;
+      while (n-- > 0) {
+        ret.add(squareMatrix.get(--i).get(j));
+      }
+      nr = nr - 2;
+      j++;
+    }
+    return ret;
   }
 
   public static void main(String[] args) {
