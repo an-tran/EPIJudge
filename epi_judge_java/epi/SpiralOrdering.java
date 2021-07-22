42fb0ea9f60e407a016456e40189829b8f1dadb7
an.tran <an.tran@big-labs.com>
2021-07-22 22:19:41 +0700

    6.17 1 loop: use direction & set boundary

--- epi_judge_java/epi/SpiralOrdering.java
+++ epi_judge_java/epi/SpiralOrdering.java
@@ -9,31 +9,24 @@ public class SpiralOrdering {
 
   public static List<Integer>
   matrixInSpiralOrder(List<List<Integer>> squareMatrix) {
-    // TODO - you fill in here.
     int nr = squareMatrix.size();
+    int n = 0;
     List<Integer> ret = new ArrayList<>();
+    int dirInx = 0;
+    int[][] direction = new int[][] { new int[] {0, 1}, new int[] {1, 0}, new int[]{0, -1}, new int[]{-1, 0} };
     int i = 0, j = 0;
-    while (nr > 0 ) {
-      int n = 0;
-      while (n++ < nr) {
-        ret.add(squareMatrix.get(i).get(j++));
+    while (n < nr * nr ) {
+      ret.add(squareMatrix.get(i).get(j));
+      squareMatrix.get(i).set(j, 0);
+      int[] dir = direction[dirInx];
+      int ni = dir[0] + i;
+      int nj = dir[1] + j;
+      if ((dirInx == 3 && ni <= 0) || nj < 0 || ni >= nr || nj >= nr || squareMatrix.get(ni).get(nj) == 0) {
+        dirInx = (dirInx + 1) % 4;
       }
-      j--;
-      n = nr - 1;
-      while (n-- > 0) {
-        ret.add(squareMatrix.get(++i).get(j));
-      }
-
-      n = nr - 1;
-      while (n-- > 0) {
-        ret.add(squareMatrix.get(i).get(--j));
-      }
-      n = nr - 2;
-      while (n-- > 0) {
-        ret.add(squareMatrix.get(--i).get(j));
-      }
-      nr = nr - 2;
-      j++;
+      i = direction[dirInx][0] + i;
+      j = direction[dirInx][1] + j;
+      n++;
     }
     return ret;
   }
