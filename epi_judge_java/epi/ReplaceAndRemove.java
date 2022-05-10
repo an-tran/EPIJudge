de1a2ce5fad9d8517e4d8711c6b2a22fa191a72c
an.tran <an.tran@big-labs.com>
2022-05-10 22:59:03 +0700

    7.4 done

--- epi_judge_java/epi/ReplaceAndRemove.java
+++ epi_judge_java/epi/ReplaceAndRemove.java
@@ -4,12 +4,41 @@ import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class ReplaceAndRemove {
 
   public static int replaceAndRemove(int size, char[] s) {
-    // TODO - you fill in here.
-    return 0;
+    int firstb = 0;
+    int na = 0;
+
+    // move b to end
+    // Example: a b b e d b c -> a e d c b b b
+    int nb = 0;
+    for (int i = 0; i < size; i++) {
+      if (s[i] != 'b') {
+        if (s[i] == 'a') na++;
+        char tmp = s[firstb];
+        s[firstb] = s[i];
+        s[i] = tmp;
+        firstb++;
+      } else {
+        nb++;
+      }
+    }
+
+
+    // replace a with 2 d
+    int replaceIdx = size + na - nb - 1; // mistake size + 2 * na - nb -1
+    for (int i = size - nb - 1; i >= 0; i--) { // mistake i > 0
+      if (s[i] == 'a') {
+        s[replaceIdx--] = 'd';
+        s[replaceIdx--] = 'd';
+      } else {
+        s[replaceIdx--] = s[i];
+      }
+    }
+    return size + na - nb;
   }
   @EpiTest(testDataFile = "replace_and_remove.tsv")
   public static List<String>
