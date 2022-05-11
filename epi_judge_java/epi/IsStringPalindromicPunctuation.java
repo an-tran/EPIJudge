bcf1463882a2280f0d44d77054859f18daa52879
an.tran <an.tran@big-labs.com>
2022-05-11 12:03:53 +0700

    7.5 done

--- epi_judge_java/epi/IsStringPalindromicPunctuation.java
+++ epi_judge_java/epi/IsStringPalindromicPunctuation.java
@@ -5,11 +5,27 @@ public class IsStringPalindromicPunctuation {
   @EpiTest(testDataFile = "is_string_palindromic_punctuation.tsv")
 
   public static boolean isPalindrome(String s) {
-    // TODO - you fill in here.
+    // compare alpha numeric letter
+    // case insensitive
+    // A man, a plan, a canal, Panama
+    String lows = s.toLowerCase();
+    int i = 0, j = lows.length() - 1;
+    while (i < j) {
+      if (Character.isLetterOrDigit(lows.charAt(i)) &&
+              Character.isLetterOrDigit(lows.charAt(j))) {
+        if (lows.charAt(i) != lows.charAt(j)) return false;
+        i++;
+        j--;
+      }
+
+      if (!Character.isLetterOrDigit(lows.charAt(i))) i++;
+      if (!Character.isLetterOrDigit(lows.charAt(j))) j--;
+    }
     return true;
   }
 
   public static void main(String[] args) {
+//    isPalindrome("7}y");
     System.exit(
         GenericTest
             .runFromAnnotations(args, "IsStringPalindromicPunctuation.java",
