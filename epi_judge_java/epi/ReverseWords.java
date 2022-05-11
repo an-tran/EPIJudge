dfbf60d00cf279d60dd2872af0e26966e015537c
an.tran <an.tran@big-labs.com>
2022-05-11 14:45:53 +0700

    7.6 done

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -5,7 +5,35 @@ import epi.test_framework.TimedExecutor;
 public class ReverseWords {
 
   public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
+    // alices likes bob
+    // bob sekil secila
+    int i =0, j = input.length - 1;
+    while (i < j) {
+      char tmp = input[i];
+      input[i] = input[j];
+      input[j] = tmp;
+      i++; j--;
+    }
+
+    int wordEndIdx = 0;
+    i = 0;
+    j = 0;
+    while (wordEndIdx < input.length) {
+      while (wordEndIdx < input.length && !Character.isSpace(input[wordEndIdx])) {
+        wordEndIdx++;
+      }
+      j = wordEndIdx - 1;
+      while (i < j) {
+        char tmp = input[i];
+        input[i] = input[j];
+        input[j] = tmp;
+        i++; j--;
+      }
+      while (wordEndIdx < input.length && Character.isSpace(input[wordEndIdx])) {
+        wordEndIdx++;
+      }
+      i = wordEndIdx;
+    }
     return;
   }
   @EpiTest(testDataFile = "reverse_words.tsv")
