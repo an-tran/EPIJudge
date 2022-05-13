55ca1692fe4a0c65ad9354a1dbcf29598f1b00ee
an-tran <an.tran@big-labs.com>
2022-05-13 11:00:01 +0700

    8.2 using dummynode

--- epi_judge_java/epi/ReverseSublist.java
+++ epi_judge_java/epi/ReverseSublist.java
@@ -6,41 +6,32 @@ public class ReverseSublist {
 
   public static ListNode<Integer> reverseSublist(ListNode<Integer> L, int start,
                                                  int finish) {
-    // TODO - you fill in here.
     if (L == null ) return L;
 
-    ListNode<Integer> head = L;
-    ListNode<Integer> p1 = L;
-    ListNode<Integer> p2 = p1.next;
-
     int p1Id = 1;
-    ListNode<Integer> dummyNode = null;
-    while (p1Id  < finish) {
-      if (p1Id < start) {
-        if (p1Id == start -1) {
+    ListNode<Integer> dummyNode = new ListNode<>(0, L);
+    ListNode<Integer> p1 = dummyNode.next;
+    ListNode<Integer> p2 = p1.next;
+    while (p1Id < finish && (p1 != null && p2 != null)) {
+      if (p1Id >= start) {
+        ListNode<Integer> tmp = p2.next;
+        p2.next = p1;
+        p1 = p2;
+        p2 = tmp;
+      } else {
+        if (p1Id == start - 1) {
           dummyNode = p1;
         }
         p1 = p2;
         p2 = p2.next;
-        p1Id++;
-        continue;
       }
-
-      ListNode<Integer> tmp = p2.next;
-      p2.next = p1;
-      p1 = p2;
-      p2 = tmp;
       p1Id++;
     }
 
-    if (dummyNode != null) {
-      dummyNode.next.next = p2;
-      dummyNode.next = p1;
-    } else {
-      head.next = p2;
-    }
+    dummyNode.next.next = p2;
+    dummyNode.next = p1;
 
-    return start == 1 ? p1 : head;
+    return start == 1 ? p1 : L;
   }
 
   public static void main(String[] args) {
