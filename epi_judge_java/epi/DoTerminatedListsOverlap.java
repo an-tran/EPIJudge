6459b8330c1ec2ffa68dfc7b5403025ff2adeac8
an.tran <an.tran@big-labs.com>
2022-05-14 17:27:25 +0700

    8.4 done

--- epi_judge_java/epi/DoTerminatedListsOverlap.java
+++ epi_judge_java/epi/DoTerminatedListsOverlap.java
@@ -7,9 +7,37 @@ public class DoTerminatedListsOverlap {
 
   public static ListNode<Integer>
   overlappingNoCycleLists(ListNode<Integer> l0, ListNode<Integer> l1) {
-    // TODO - you fill in here.
+    int len0 = 0;
+    int len1 = 0;
+    ListNode<Integer> head0 = l0;
+    ListNode<Integer> head1 = l1;
+    while (l0 != null) {
+      len0++;
+      l0 = l0.next;
+    }
+    while (l1 != null) {
+      len1++;
+      l1 = l1.next;
+    }
+
+    ListNode<Integer> longlist = len0 > len1 ? head0 : head1;
+    ListNode<Integer> shortlist = len0 > len1 ? head1 : head0;
+    int longlen = Math.max(len0, len1);
+    int shortlen = Math.min(len0, len1);
+    while (shortlen > 0 ) {
+      if (longlen > shortlen) {
+        longlist = longlist.next;
+        longlen--;
+      } else {
+        if (longlist == shortlist) return longlist;
+        longlist = longlist.next;
+        shortlist = shortlist.next;
+        shortlen--; longlen--;
+      }
+    }
     return null;
   }
+
   @EpiTest(testDataFile = "do_terminated_lists_overlap.tsv")
   public static void
   overlappingNoCycleListsWrapper(TimedExecutor executor, ListNode<Integer> l0,
