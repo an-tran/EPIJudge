bc163d689f2caa49e811e968b1533887d5bb6e14
an.tran <an.tran@big-labs.com>
2022-05-14 18:58:20 +0700

    8.7 done

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -6,8 +6,23 @@ public class DeleteKthLastFromList {
 
   // Assumes L has at least k nodes, deletes the k-th last node in L.
   public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> dummy = new ListNode<>(0, L);
+    ListNode<Integer> iter = L;
+    while (k > 0) {
+      iter = iter.next;
+      k--;
+    }
+
+    while (iter != null) {
+      dummy = dummy.next;
+      iter = iter.next;
+    }
+
+    ListNode<Integer> knode = dummy.next;
+    dummy.next = knode.next;
+
+    // dummy -> 1 2 3 4 5 6 7
+    return knode == L ? dummy.next : L;
   }
 
   public static void main(String[] args) {
