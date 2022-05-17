781731999ffd13149e9d6960b624f245c93f6688
an.tran <an.tran@big-labs.com>
2022-05-17 22:44:54 +0700

    9.2 done

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,12 +1,61 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class EvaluateRpn {
   @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
   public static int eval(String expression) {
     // TODO - you fill in here.
-    return 0;
+    // 1. parse string form end to begin and put to stack
+    // -6,12,+
+    final String SEP = ",";
+    String[] tokens = expression.split(",");
+    Deque<String> stack = new LinkedList<>();
+
+    for (int i = tokens.length - 1; i >= 0; i--) {
+      stack.offerFirst(tokens[i]);
+    }
+
+    // 2. evaluate
+    // 1,2,*,6,3,/,+
+    Deque<Integer> operands = new LinkedList<>();
+
+    int left = 0;
+    int right = 0;
+    while (!stack.isEmpty()) {
+      String ele = stack.pollFirst();
+      switch (ele) {
+        case "+":
+          right = operands.pollFirst();
+          left = operands.pollFirst();
+          operands.offerFirst(left + right);
+          break;
+        case "-":
+          right = operands.pollFirst();
+          left = operands.pollFirst();
+          operands.offerFirst(left - right);
+          break;
+        case "*":
+          right = operands.pollFirst();
+          left = operands.pollFirst();
+          operands.offerFirst(left * right);
+          break;
+        case "/":
+          right = operands.pollFirst();
+          left = operands.pollFirst();
+          operands.offerFirst(left / right);
+          break;
+        default:
+          operands.offerFirst(Integer.parseInt(ele));
+          break;
+      }
+    }
+
+    return operands.pollFirst();
   }
 
   public static void main(String[] args) {
