579dcd2b7e4983031a50c9ad15e0649ae0de85dd
an-tran <an.tran@big-labs.com>
2022-05-26 20:57:37 +0700

    11.1 done

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -2,14 +2,29 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
-import java.util.List;
+import java.util.*;
+
 public class SortedArraysMerge {
   @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
+    PriorityQueue<List<Integer>> maxheap = new PriorityQueue(sortedArrays.size(), (Comparator<List<Integer>>) (a, b) -> Integer.compare(b.get(b.size() - 1), a.get(a.size() - 1)));
+
+    for (List<Integer> l : sortedArrays) {
+      maxheap.add(l);
+    }
+
+    List<Integer> ret = new ArrayList<>();
+    while (maxheap.size() > 0) {
+      List<Integer> l = maxheap.poll();
+      ret.add(l.remove(l.size() - 1));
+      if (l.size() > 0) maxheap.add(l);
+    }
+
+
+    Collections.reverse(ret);
+    return ret;
   }
 
   public static void main(String[] args) {
