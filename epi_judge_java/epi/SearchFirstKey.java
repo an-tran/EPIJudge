b6fc33ebbeefd3181c6adf960631573768c93ec7
an-tran <an.tran@big-labs.com>
2022-05-28 16:04:52 +0700

    Using Collections.binarySearch()

--- epi_judge_java/epi/SearchFirstKey.java
+++ epi_judge_java/epi/SearchFirstKey.java
@@ -2,13 +2,21 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
 public class SearchFirstKey {
   @EpiTest(testDataFile = "search_first_key.tsv")
 
   public static int searchFirstOfK(List<Integer> A, int k) {
-    // TODO - you fill in here.
-    return 0;
+    int idx = Collections.binarySearch(A, k);
+    int ret = idx;
+    while (idx >= 0) {
+      List<Integer> subList = A.subList(0, idx);
+      ret = idx;
+      idx = Collections.binarySearch(subList, k);
+    }
+
+    return ret < 0 ? -1 : ret;
   }
 
   public static void main(String[] args) {
