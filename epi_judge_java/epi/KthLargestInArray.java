c6efe0464c7c8405a9a3329f6ab424158ef0c2b5
an-tran <an.tran@big-labs.com>
2022-05-31 15:15:38 +0700

    12.8 better but complex

--- epi_judge_java/epi/KthLargestInArray.java
+++ epi_judge_java/epi/KthLargestInArray.java
@@ -2,36 +2,54 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Random;
 public class KthLargestInArray {
   // The numbering starts from one, i.e., if A = [3,1,-1,2] then
   // findKthLargest(1, A) returns 3, findKthLargest(2, A) returns 2,
   // findKthLargest(3, A) returns 1, and findKthLargest(4, A) returns -1.
   @EpiTest(testDataFile = "kth_largest_in_array.tsv")
   public static int findKthLargest(int k, List<Integer> A) {
-    int upperBound = Integer.MAX_VALUE;
-    int ret = 0;
-    while (k-- > 0) {
-      ret = findMax(A, 0, A.size() - 1, upperBound);
-      upperBound = ret;
+    Random rand = new Random();
+    int start = 0, end = A.size() - 1;
+    int next = 1;
+    int pivotIdx = rand.nextInt(A.size());
+    while (true) { //TODO: update found
+      Collections.swap(A, start, pivotIdx);
+      int tail = end;
+      while (next <= tail) {
+        if (A.get(tail) <= A.get(start)) {
+          Collections.swap(A, next, tail);
+          next++;
+        } else {
+          tail--;
+        }
+      }
+      int biggerLen = end - tail;
+      if (biggerLen == k -1) {
+        return A.get(start);
+      } else if (biggerLen > k - 1) {
+        start = next;
+        if (biggerLen == 1) {
+          return A.get(start);
+        }
+        pivotIdx = start + rand.nextInt(biggerLen - 1);
+      } else {
+        k = k - biggerLen - 1;
+        start++;
+        end = tail;
+        if (start >= end) {
+          System.out.println("Invalid state, k = " + k);
+          return A.get(start);
+        }
+        pivotIdx = start + rand.nextInt(end - start);
+      }
+      next = start + 1;
+
     }
-    return ret;
   }
 
-  public static int findMax(List<Integer> A, int start, int end, int upbound) {
-    if (start == end) return A.get(start);
-    if (start > end) return Integer.MIN_VALUE;
-    int middle = start + (end - start)/2;
-    int maxVal = Math.min(A.get(middle), upbound);
-    if (A.get(middle) >= upbound) {
-      maxVal = Integer.MIN_VALUE;
-    }
-    int left =  findMax(A, start, middle - 1, upbound);
-    int right = findMax(A, middle + 1, end, upbound);
-    maxVal = Math.max(maxVal, left >= upbound ? Integer.MIN_VALUE : left);
-    maxVal = Math.max(maxVal, right >= upbound ? Integer.MIN_VALUE : right);
-    return maxVal;
-  }
 
   public static void main(String[] args) {
     System.exit(
