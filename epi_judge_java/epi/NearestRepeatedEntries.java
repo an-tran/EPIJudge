19a42cb2aa13ef84b04cec0be9d7f9ffe63b815a
an-tran <an.tran@big-labs.com>
2022-06-06 17:00:03 +0700

    13.6 doen

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -2,13 +2,45 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 public class NearestRepeatedEntries {
-  @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
+  private static class Pair {
+    int x;
+    int y;
+    int minDist = Integer.MAX_VALUE;
+    public Pair(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public void setData(int x, int y) {
+      this.x = x;
+      this.y = y;
+      minDist = Math.min(minDist, y - x);
+    }
+  }
+
+  @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
   public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
+    int ret = Integer.MAX_VALUE;
+    Map<String, Pair> wordDist = new HashMap<>();
+    for (int i = 0 ; i < paragraph.size(); i++) {
+      String word = paragraph.get(i);
+
+      if (wordDist.containsKey(word)) {
+        Pair minPair = wordDist.get(word);
+        minPair.setData(minPair.y, i);
+        ret = Math.min(ret, minPair.minDist);
+      } else {
+        wordDist.put(word, new Pair(i, i));
+      }
+    }
+
+    return ret == Integer.MAX_VALUE ? -1 : ret;
   }
 
   public static void main(String[] args) {
