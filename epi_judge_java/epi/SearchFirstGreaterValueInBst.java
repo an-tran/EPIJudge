e8049e98165d0d2daf9e7d1b71b63032b2c64cbf
an.tran <7043902+an-tran@users.noreply.github.com>
2022-07-04 15:53:39 +0700

    15.2 done - use recursive

--- epi_judge_java/epi/SearchFirstGreaterValueInBst.java
+++ epi_judge_java/epi/SearchFirstGreaterValueInBst.java
@@ -1,13 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
 public class SearchFirstGreaterValueInBst {
 
   public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
                                                        Integer k) {
-    // TODO - you fill in here.
-    return null;
+    return findHelper(tree, k, null);
   }
+
+  private static BstNode<Integer> findHelper(BstNode<Integer> tree, Integer k, BstNode<Integer> greaterThanK) {
+    if (tree == null) return greaterThanK;
+
+    if (tree.data <= k) {
+      return findHelper(tree.right, k, greaterThanK);
+    } else if (tree.data > k) {
+      if (greaterThanK == null) greaterThanK = tree;
+      else greaterThanK = greaterThanK.data <= tree.data ? greaterThanK : tree;
+      
+      return findHelper(tree.left, k, greaterThanK);
+    }
+
+    return greaterThanK;
+  }
+
   @EpiTest(testDataFile = "search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
                                                  Integer k) {
