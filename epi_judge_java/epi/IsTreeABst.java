f51dcfebb2788864e22879f0203098553ada4a7f
an.tran <7043902+an-tran@users.noreply.github.com>
2022-07-04 11:17:19 +0700

    15.1 done - use recursive & compare parent with the max left and min right

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -1,12 +1,44 @@
 package epi;
+import java.lang.reflect.Constructor;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class IsTreeABst {
-  @EpiTest(testDataFile = "is_tree_a_bst.tsv")
+  public static class SubTreeResult {
+    boolean isbst;
+    BinaryTreeNode<Integer> minval;
+    BinaryTreeNode<Integer> maxval;
 
+    public SubTreeResult(boolean isbst, BinaryTreeNode<Integer> leftmost, BinaryTreeNode<Integer> rightmost) {
+      this.isbst = isbst;
+      minval = leftmost;
+      maxval = rightmost;
+    }
+  }
+
+  @EpiTest(testDataFile = "is_tree_a_bst.tsv")
   public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    SubTreeResult ret = isSubTreeBST(tree);
+    return ret.isbst;
+  }
+
+  public static SubTreeResult isSubTreeBST(BinaryTreeNode<Integer> tree) {
+    if (tree == null) return new SubTreeResult(true, null, null);
+
+    SubTreeResult leftTree = null, rightTree = null;
+    leftTree = isSubTreeBST(tree.left);
+    rightTree = isSubTreeBST(tree.right);
+
+    if (!leftTree.isbst || (leftTree.maxval != null && tree.data.compareTo(leftTree.maxval.data) < 0)) {
+      return new SubTreeResult(false, null, null);
+    }
+
+    if (!rightTree.isbst || (rightTree.minval != null && tree.data.compareTo(rightTree.minval.data) > 0)) {
+      return new SubTreeResult(false, null, null);
+    }
+
+    SubTreeResult ret = new SubTreeResult(true, leftTree.minval != null ? leftTree.minval : tree, rightTree.maxval != null ? rightTree.maxval : tree);
+    return ret;
   }
 
   public static void main(String[] args) {
