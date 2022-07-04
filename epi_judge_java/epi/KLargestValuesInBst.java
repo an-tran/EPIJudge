1a919360163e40dfbb2e3501b4d0eb60fb1a0866
an.tran <7043902+an-tran@users.noreply.github.com>
2022-07-04 17:10:04 +0700

    15.3 done - recursive

--- epi_judge_java/epi/KLargestValuesInBst.java
+++ epi_judge_java/epi/KLargestValuesInBst.java
@@ -3,6 +3,7 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.GenericTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -10,9 +11,20 @@ public class KLargestValuesInBst {
   @EpiTest(testDataFile = "k_largest_values_in_bst.tsv")
 
   public static List<Integer> findKLargestInBst(BstNode<Integer> tree, int k) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+    List<Integer> ret = new ArrayList<>();
+    findKLargest(tree, k, ret);
+    return ret;
   }
+  
+  private static void findKLargest(BstNode<Integer> tree, int k, List<Integer> ret) {
+    // if (ret.size() == k) return;
+    if (tree == null) return;
+    findKLargest(tree.right, k, ret);
+    if (ret.size() == k) return;
+    ret.add(tree.data);
+    findKLargest(tree.left, k, ret);
+  }
+
   @EpiTestComparator
   public static boolean comp(List<Integer> expected, List<Integer> result) {
     if (result == null) {
